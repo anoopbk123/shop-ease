d2ee926b24bab91fb154145635b9419f808df794
Anoop B K <anoopkavalakkal@gmail.com>
2024-02-06 17:05:23 +0530

    footer added

--- shop-ease/src/components/footer/Footer.jsx
+++ shop-ease/src/components/footer/Footer.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import footerData from "../../data/footerData";
 
 export default function Footer() {
   return (
-    <footer className='footer-container bg-dark text-light'>
-      <h2>SHOP EASE</h2>
+    <footer className="footer-container bg-dark text-light pt-3 position-relative">
+      {/* || scroll button */}
+      <Link
+        className="position-absolute end-0 mx-3"
+        onClick={() => {
+          window.scroll(0, 0);
+        }}
+      >
+        <i className="bi bi-arrow-up"></i>Move to top
+      </Link>
+
       <div className="container">
-        <h6 className="h6">
-            Connect With Us
-        </h6>
+        <div className="row">
+          <h5 className="col-12">SHOP EASE</h5>
+        </div>
+        <div className="container">
+          <div className="row">
+            {/* || Customer Support */}
+            <div className="col-12 col-md-7 my-3 mt-md-0 text-center">
+              <h6 className="h6 text-center">
+                <img
+                  src={footerData.customerSupport.customerSupportIcon}
+                  alt=""
+                  className="mx-1"
+                />
+                 Customer Support
+              </h6>
+              <ul className="nav flex-column flex-sm-row">
+                {footerData.customerSupport.customerSupportNumber.map(
+                  (value) => (
+                    <li className="nav-item"><i className={`${value.icon}`}>{value.text}</i>
+                       <Link className="mx-5 nav-link text-light" to={value.link}>
+                         {value.number}
+                      </Link>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+             {/* || Connect With Us */}
+            <div className="col-12 col-md-5 text-center">
+              <h6 className="h6 text-center">Connect With Us</h6>
+              <div>
+                {footerData.connectUs.map((value) => (
+                  <a href={value.link} className="mx-2 text-light">
+                    <i className={value.icon}></i>
+                  </a>
+                ))}
+              </div>
+              <div className="mt-3">
+                &copy; 2024 Shop ease, All rights reserved
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
---
+++ shop-ease/src/data/footerData.js
@@ -0,0 +1,59 @@
+import customerSupportIcon from './images/customer-service.gif'
+const footerData = {
+    connectUs:[
+        {
+            link:'#facebook',
+            icon:'bi bi-facebook'
+        },
+        {
+            link:'#instagram',
+            icon:'bi bi-instagram'
+        },
+        {
+            link:'mailto:anoopkavalakkal@gmail.com',
+            icon:'bi bi-envelope-at'
+        },
+        {
+            link:'tel:911',
+            icon:'bi bi-telephone-inbound'
+        },
+        {
+            link:'#threads',
+            icon:'bi bi-threads-fill'
+        },
+        {
+            link:'#twitter-x',
+            icon:'bi bi-twitter-x'
+        },
+        {
+            link:'#whatsapp',
+            icon:'bi bi-whatsapp'
+        },
+    ],
+    customerSupport:{
+        customerSupportIcon:customerSupportIcon,
+        customerSupportNumber:[
+            {
+                link:'tel:+12121212121112',
+                text:` +121212121212 `,
+                icon:'bi bi-telephone-inbound'
+            },
+            {
+                link:'tel:+121212356735',
+                text:` +1212124563 `,
+                icon:'bi bi-telephone-inbound'
+            },
+            {
+                link:'mailto:abc@gmail.com',
+                text:` abc@gmail.com `,
+                icon:'bi bi-envelope-at'
+            },
+            {
+                link:'tel:+12121212121112',
+                text:` +121212121212 `,
+                icon:'bi bi-whatsapp'
+            },
+        ]
+    }
+}
+export default footerData;
